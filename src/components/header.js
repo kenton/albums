b2ac7566a55bda628d4531b03ab333543a99b682
Kenton Newby <kentonnewby@gmail.com>
2018-11-16 08:57:32 -0500

    Initial commit
    * adds header component

---
+++ src/components/header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    setHeight: 60,
+    paddingTop: 40,
+    paddingBottom: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2},
+    shadowOpacity: .2
+  },
+  textStyle: {
+    fontSize: 20
+  }
+};
+
+export default Header;
