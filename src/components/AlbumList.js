5ae45e5a5432540f15ec2344a57148be39c31e46
Kenton Newby <kentonnewby@gmail.com>
2018-11-18 22:12:01 -0500

    misc code cleanup

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -9,22 +9,21 @@ class AlbumList extends Component {
   componentWillMount() {
     var remoteURL = 'http://rallycoding.herokuapp.com/api/music_albums';
     axios.get(remoteURL).then(response => this.setState({ albums: response.data}));
-  }
+  };
 
   renderAlbums(){
     return this.state.albums.map(album =>
       <AlbumDetail key={album.title} album={album} />
     );
-  }
+  };
 
   render() {
-    console.log(this.state);
     return (
       <ScrollView>
         {this.renderAlbums()}
       </ScrollView>
     );
-  }
+  };
 };
 
 export default AlbumList;
